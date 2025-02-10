5722f13847c082c6399846e746acb13f2b3d5cb7
Jacek Wojnarowski <139857755+jwojnarowskiDTP@users.noreply.github.com>
2025-02-10 17:20:39 +0100

    init rizonedb
    Added init script to start-patroni.sh

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -3,6 +3,12 @@
 # For security measures, daemons should not be run as sudo. Execute patroni as the non-sudo user: snap_daemon.
 export LOCPATH="${SNAP}"/usr/lib/locale
 
+execute_query() {
+    local query=$1
+    /snap/bin/charmed-postgresql.psql -h localhost -p 5432 -U postgres -c "$query" -t -A
+}
+
+
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/etc/patroni
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_COMMON/postgresql
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/postgresql
@@ -14,3 +20,19 @@ fi
 
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
+
+#rizone setup database
+while true; do
+    # check leader statuspg_is_in_recovery()
+    result=$(execute_query "SELECT pg_is_in_recovery();")
+    # Sprawdź wynik
+    if [ "$result" == "f" ]; then
+        # create database and user
+        execute_query "CREATE ROLE rizone WITH LOGIN PASSWORD 'rizone';"
+        execute_query "CREATE DATABASE rizone OWNER rizone;"
+        break
+    else
+        echo "Waiting to postgresql Leader..."
+        sleep 10
+    fi
+done
